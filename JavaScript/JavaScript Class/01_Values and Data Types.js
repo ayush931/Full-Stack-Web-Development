c3ec1081ae3c6558c492ae77b011c4ecd2fd0ec9
ayush931 <ayushkumar9315983@gmail.com>
2023-04-04 11:51:47 +0530

    javaScript

---
+++ JavaScript/JavaScript Class/01_Values and Data Types.js
@@ -0,0 +1,64 @@
+// Hello World in Node.js
+
+console.log("Ayush");
+console.log(this);
+
+// Values and Data types
+
+// - Primitive data types
+// - number: 2, 3, 4, 4.5, 24
+
+// - String: "PW Skills" or 'PW Skills'
+
+// - Boolean: true, false
+
+// - Null: nothing
+
+// - Undefined
+
+
+// - Non Primitive data types
+// - Array
+
+let name1 = "Ayush"
+let name2 = "Sushant"
+let name3 = "Rohit"
+let name4 = "Kaif"
+
+// Array
+let names = ["Ayush", "Sushant", "Rohit", "Kaif", 2, 3, false]
+
+// - object: {}
+let myName = "Ayush" 
+
+ // let for variable (if you want to change)
+// const for constant (if you don't want to change)
+
+myName = "Ayush Kumar"
+
+
+let highScore = 300
+console.log(highScore);
+
+let pwSkillsCoursePrice = 3500
+console.log(pwSkillsCoursePrice);
+
+pwSkillsCoursePrice = 1000
+console.log(pwSkillsCoursePrice);
+
+
+let userName = "Ayush"
+console.log(userName);
+
+let pwCoursePrice = 200
+let gstOnCourses = 36
+let finalPurchaseAmount = pwCoursePrice + gstOnCourses
+console.log("Final amount to be paid:", finalPurchaseAmount);
+
+let pwDsaCourse = 500
+console.log("Final price of DSA course:", pwDsaCourse + gstOnCourses);
+
+console.log("Answer is:", 5 % 2); // % gives the remainder.
+console.log("Answer is:", 5 / 2);
+console.log("Answer is:", 5 ** 2); // ** gives the power.
+
