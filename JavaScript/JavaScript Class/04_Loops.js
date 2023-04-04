2c410dc4ea5f4917fd279f29b780b61190f8e1ab
ayush931 <ayushkumar9315983@gmail.com>
2023-04-04 14:02:02 +0530

    JavaScript

---
+++ JavaScript/JavaScript Class/04_Loops.js
@@ -0,0 +1,24 @@
+// Loops
+// do while, while, for
+
+// for
+
+for (let i = 0; i< 5; i++) {
+    console.log(i);
+}
+
+// while (init, condition, increment/decrement)
+
+let i = 0
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// Do while (run atleast one time)
+
+let x = 0
+do {
+    console.log("Hello World");
+    x++;
+} while (x < 5)
---
+++ JavaScript/JavaScript Class/05_Ternary Condition.js
@@ -0,0 +1,5 @@
+// Ternary OP/ Ternary Condition
+// condition ? True : False
+
+let isLoggedIn = false
+isLoggedIn ? console.log("Home Page") : console.log("Login");
