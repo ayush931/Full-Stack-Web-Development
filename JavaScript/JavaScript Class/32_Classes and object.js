782536fe319fc12a865a7fa5838f61a7691788a9
ayush931 <ayushkumar9315983@gmail.com>
2023-05-12 11:12:46 +0530

    JavaScript

--- JavaScript/JavaScript Class/32_Classes and object.js
+++ JavaScript/JavaScript Class/32_Classes and object.js
@@ -1,5 +1,10 @@
 class product {
     // Properties -> variable -> data number
+    // name;
+    // price:
+    // rating;
+
+    #rating; // # makes this private. call it using the # before it.
 
     name;
     price;
@@ -9,17 +14,21 @@ class product {
         console.log("calling the constructor");
         this.name = n
         this.price = p
-        this.rating = r
+        this.#rating = r
         // return 10; if you are returning primitive then it will be avoided in constructor.
         // return {x: 20, y: 30} if you return non primitive then it will be returned.
         // return this
+    }
+
+    static custom () {
+        console.log("calling a static method");
     }
 
     // behaviour -> functions -> member functions
 
     display () {
         console.log("this refers to;", this);
-        console.log("displaying the current product", this.name, this.price, this.rating);
+        console.log("displaying the current product", this.name, this.price, this.#rating);
     }
 }
 
@@ -27,5 +36,9 @@ const p = new product ("Iphone", 100000, 5) // new creates an empty plain object
 // In the above piece of code we are calling the constructor method
 
 console.log(p);
+p.name = "samsung s23"
+console.log(p.name);
+console.log(p.rating);
 
 p.display ()
+product.custom () // call using the class name.
